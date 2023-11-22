fd0bf8429222a3106ed8b4fb0df63a417e21bced
tereneka <teren-eka@yandex.ru>
2023-11-22 08:23:30 +0300

    feat: add .env

--- src/db/firebaseConfig.ts
+++ src/db/firebaseConfig.ts
@@ -3,16 +3,25 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey:
-    'AIzaSyA6pW53wJLEcxCovwvqzgbQtCn1gzZuGcM',
-  authDomain: 'kudry-e905c.firebaseapp.com',
-  projectId: 'kudry-e905c',
-  storageBucket: 'kudry-e905c.appspot.com',
-  messagingSenderId: '869626329614',
-  appId:
-    '1:869626329614:web:59eaa85182391bf64a1a5f',
-  measurementId: 'G-8R2L321C50',
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:
+    REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
